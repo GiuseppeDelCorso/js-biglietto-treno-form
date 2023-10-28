98281a8e814b0f3420af9d461069e5a009809531
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-10-28 17:13:52 +0200

    Salvataggio

--- main.js
+++ main.js
@@ -1,39 +1,55 @@
-const age = parseInt( prompt("Inserire l'età del viaggiatore") );
-const distance = parseInt( prompt("Inserire la distanza del viaggio in km") );
-const element = document.getElementById("message");
-const costoPerKm = 0.21;
-let message = "";
+let element = document.getElementById("message");
 let btn = document.querySelector("#bottoneGenera")
-let risultato = document.getElementById("#risultato")
-let valore = document.querySelector("#nomeCognome")
+let risultatoNC = document.getElementById("risultatoNC")
+let risultatoKM = document.getElementById("risultatoKM")
+let risultatoEta = document.getElementById("risultatoEta")
+let valoreNC = document.querySelector("#nomeCognome")
+let valoreKM = document.querySelector("#kmDaPercorrere")
+let valoreEta = document.getElementById("Eta")
 let nomeDellUtente = document.getElementById("nomeDellUtente")
+let CP = document.querySelector("#codiceCP")
+let numeroCP = 92134;
+const costoPerKm = 0.21;
+const age = document.querySelector("#Eta").value;
+const distance = document.querySelector("#kmDaPercorrere").value;  
+let costo = distance * costoPerKm;
 
 
-if( Number.isFinite(age) && Number.isFinite(distance) ) {
-    
-
-    let costo = distance * costoPerKm;
-    console.log("Costo iniziale: ", costo);
-
-
-    if(age < 18) {
-        costo = costo * 0.8;
-        message = `Scontato del 20% `;
-
-
-    } else if(age >= 65) {
-        costo = costo * 0.6;      
-        message = `Scontato del 40% `;
-    }
-
-
-    message += `Costo biglietto: ${costo.toFixed(2)} €`;
-
-}
-element.innerHTML = message;
 btn.addEventListener("click", function() {
     //Questo codice verrà eseguito solo quando l'utente cliccherà sul button
     //E mostrerà il valore che abbiamo nell'input IN QUEL MOMENTO
-    risultato = ("nome utente: " + valore.value);
-    nomeDellUtente.innerHTML = risultato
+    risultatoNC = valoreNC.value;
+    nomeDellUtente.innerHTML = risultatoNC
+    risultatoKM = valoreKM.value;
+    costoBiglietto.innerHTML = risultatoKM
+    risultatoEta = valoreEta.value;
+    EtaViaggiatore.innerHTML = risultatoEta
+    CP.innerHTML = numeroCP
+    element.innerHTML = message;
+    
+    
+    if( Number.isFinite(age) && Number.isFinite(distance) ) {
+    
+
+        let costo = distance * costoPerKm;
+        console.log("Costo iniziale: ", costo);
+    
+    
+        if(age < 18) {
+            costo = costo * 0.8;
+            message = `Scontato del 20% `;
+    
+    
+        } else if(age >= 65) {
+            costo = costo * 0.6;      
+            message = `Scontato del 40% `;
+        }
+    
+    
+        message += `Costo biglietto: ${costo.toFixed(2)} €`;
+    
+    }
+    
 });
+
+
